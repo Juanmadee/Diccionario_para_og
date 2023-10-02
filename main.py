01c546d374f57db6ad3dac11673cb97aa68f6863
Juanmadee <146781830+Juanmadee@users.noreply.github.com>
2023-10-02 18:59:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+diccionario = {
+    "vpi": "Abreviatura de Va para ahí, usado como un OK o un Dale",
+    "lol": "Puede ser usada como una abreviatura de el juego league of legends, o también como abreviatura de laughing out loud",
+    "cringe": "Algo que provoca vergüenza ajena"
+}
+while True:
+    word = input("Introduce la palabra que quieras buscar:").lower()
+    if word in  diccionario.keys():
+        print(diccionario[word])
+    else:
+        print("Esa palabra no está en el diccionario!")
+    cont = input("Deseas seguir buscando palabras? (s/n)").lower()
+    if cont == "n":
+        print("Gracias por usar el diccionario!")
+        break
+    
